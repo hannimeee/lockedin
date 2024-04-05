086dd455e5e2bc25b73d4236bad7335695f8f14f
Surya-Sathya <SURYASATHYA20@GMAIL.COM>
2024-04-06 00:11:09 +1100

    major change

--- FaceRec/FaceVideo3.js
+++ FaceRec/FaceVideo3.js
@@ -8,7 +8,6 @@ let faceDetectionInterval;
 let currentTimeIn;
 const timelog = [];
 
-
 Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
@@ -46,45 +45,34 @@ function getLabeledFaceDescriptions() {
     })
   );
 }
-function getTop(l)
-{
-    return l.map((a) => a.y).reduce((a, b) => Math.min(a, b));
-}
 
-function getMeanPosition(l)
-{
-    return l.map((a) => [a.x, a.y]).reduce((a, b) => [a[0] + b[0], a[1] + b[1]]).map((a) => a / l.length);
-}
-function xyFaceDetect(detection)
-{
-    const { landmarks, detection: det } = detection; // Destructure for clarity
-        
-    const eyeRight = getMeanPosition(landmarks.getRightEye());
-    const eyeLeft = getMeanPosition(landmarks.getLeftEye());
-    const nose = getMeanPosition(landmarks.getNose());
-    const mouth = getMeanPosition(landmarks.getMouth())[1];
-    const jaw = getTop(landmarks.getJawOutline());
-    
-    const boxHeight = det.box.height;
-    const boxWidth = det.box.width;
+function xyFaceDetect(detection) {
+  const { landmarks, detection: det } = detection; // Destructure for clarity
 
-    const rx = (jaw - mouth) / boxHeight;
-    const ry = (eyeLeft[0] + (eyeRight[0] - eyeLeft[0]) / 2 - nose[0]) / boxWidth;
-    const ryNumber = parseFloat(ry.toFixed(3));
-    const rxNumber = parseFloat(rx.toFixed(3));
-    //console.log(`Jaw: ${jaw}, Mouth: ${mouth}, Box Height: ${boxHeight}`);
+  const eyeRight = getMeanPosition(landmarks.getRightEye());
+  const eyeLeft = getMeanPosition(landmarks.getLeftEye());
+  const nose = getMeanPosition(landmarks.getNose());
+  const mouth = getMeanPosition(landmarks.getMouth())[1];
+  const jaw = getTop(landmarks.getJawOutline());
 
-    //console.log(rxNumber,ryNumber);
-    return {rxNumber,ryNumber};
+  const boxHeight = det.box.height;
+  const boxWidth = det.box.width;
+
+  const rx = (jaw - mouth) / boxHeight;
+  const ry = (eyeLeft[0] + (eyeRight[0] - eyeLeft[0]) / 2 - nose[0]) / boxWidth;
+  const ryNumber = parseFloat(ry.toFixed(3));
+  const rxNumber = parseFloat(rx.toFixed(3));
+
+  return { rxNumber, ryNumber };
 }
 
 async function startRecordingWithFaceDetection() {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   video.srcObject = stream;
-  // Specify MIME type for recording
+
   const options = MediaRecorder.isTypeSupported('video/mp4; codecs="avc1.42E01E"')
-                  ? { mimeType: 'video/mp4; codecs="avc1.42E01E"' }
-                  : { mimeType: 'video/webm' };
+    ? { mimeType: 'video/mp4; codecs="avc1.42E01E"' }
+    : { mimeType: 'video/webm' };
   mediaRecorder = new MediaRecorder(stream, options);
   recordedBlobs = [];
   mediaRecorder.ondataavailable = (event) => {
@@ -93,7 +81,6 @@ async function startRecordingWithFaceDetection() {
     }
   };
   mediaRecorder.start(10);
-  // Start face detection
   startFaceDetection();
   console.log('MediaRecorder started', mediaRecorder);
 }
@@ -116,23 +103,21 @@ async function startFaceDetection() {
     const optimizedX = 0;
     const optimizedY = 0;
     const filteredDetections = detections.filter(detection => {
-        const {rxNumber,ryNumber} = xyFaceDetect(detection);
-        return ryNumber > optimizedY-0.5 && ryNumber < optimizedY + 0.5 && 
-                rxNumber > optimizedX-0.5 && rxNumber < optimizedX + 0.5;
+      const { rxNumber, ryNumber } = xyFaceDetect(detection);
+      return ryNumber > optimizedY - 0.5 && ryNumber < optimizedY + 0.5 &&
+        rxNumber > optimizedX - 0.5 && rxNumber < optimizedX + 0.5;
     });
-    
+
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
-    // Increment both face detected and not detected counters
     if (filteredDetections.length > 0) {
       faceDetectedCount++;
     } else {
       faceNotDetectedCount++;
     }
 
-    // Update the counters in the HTML
     document.getElementById("face-detected-count").innerText = faceDetectedCount;
     document.getElementById("face-not-detected-count").innerText = faceNotDetectedCount;
 
@@ -147,94 +132,93 @@ async function startFaceDetection() {
       drawBox.draw(canvas);
     });
     updateLockedInPercentage();
-
   }, 100);
 };
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  let canvas; // Define canvas variable outside of the event listener
+document.addEventListener('DOMContentLoaded', function () {
+  let canvas;
   const startButton = document.getElementById('startButton');
   const pauseButton = document.getElementById('pauseButton');
   const resetButton = document.getElementById('resetButton');
-  // const stopButton = document.getElementById('stopButton');
-  let isDetectionStarted = false; // Track if detection has started
+  let isDetectionStarted = false;
+  let startTime;
 
   startButton.addEventListener('click', async () => {
     pauseButton.disabled = false;
     startButton.disabled = true;
-    resetButton.disabled = true; // Disable reset button when detection starts
+    resetButton.disabled = true;
     startRecordingWithFaceDetection();
-    isDetectionStarted = true; // Set detection started flag to true
-    startButton.textContent = 'Resume'; // Change button text to 'Resume'
+    isDetectionStarted = true;
+    startButton.textContent = 'Resume';
+    startTime = new Date();
     timeIn();
   });
 
   pauseButton.addEventListener('click', () => {
-    timeOut(currentPercentage);
+    const currentTimeOut = new Date();
+    const elapsedSeconds = Math.round((currentTimeOut - startTime) / 1000);
+    const efficiency = calculateEfficiency(faceDetectedCount, elapsedSeconds);
+    timeOut(efficiency);
     clearInterval(faceDetectionInterval);
 
     startButton.disabled = false;
     pauseButton.disabled = true;
-    resetButton.disabled = false; // Enable reset button when detection stops
+    resetButton.disabled = false;
 
-    if (canvas) { // Check if canvas is defined before removing it
-      canvas.remove(); // Remove the canvas from the DOM
-      canvas = null; // Reset the canvas variable
+    if (canvas) {
+      canvas.remove();
+      canvas = null;
     } else {
-      canvas = document.querySelector('canvas'); // Retrieve the canvas element
+      canvas = document.querySelector('canvas');
       if (canvas) {
-        canvas.remove(); // Remove the canvas from the DOM
-        canvas = null; // Reset the canvas variable
+        canvas.remove();
+        canvas = null;
       }
     }
   });
-  
+
   resetButton.addEventListener('click', () => {
     faceDetectedCount = 0;
     faceNotDetectedCount = 0;
     updateLockedInPercentage();
-    startButton.textContent = 'Start'; // Change button text back to 'Start' after reset
+    startButton.textContent = 'Start';
   });
 });
 
+function calculateEfficiency(faceDetectedCount, elapsedSeconds) {
+  const lockedInPercentage = (faceDetectedCount / (faceDetectedCount + faceNotDetectedCount)) * 100;
+  const efficiency = Math.round((lockedInPercentage * elapsedSeconds) / 100);
+  return efficiency;
+}
 
-// Update the "Locked in %" stat
 function updateLockedInPercentage() {
   const totalFrames = faceDetectedCount + faceNotDetectedCount;
   const percentage = totalFrames > 0 ? ((faceDetectedCount / totalFrames) * 100).toFixed(2) : 0;
   document.getElementById("locked-in-percentage").textContent = percentage;
 }
 
-function timeIn(){
+function timeIn() {
   currentTimeIn = new Date();
 }
+
 function timeOut(efficiency) {
   const currentTimeOut = new Date();
-
-  // Assuming formatTime is a function that formats your Date objects
   const formattedIn = formatTime(currentTimeIn);
   const formattedOut = formatTime(currentTimeOut);
-
-  // Add a new object to the timelog array
   timelog.push({
     currentTimeIn: formattedIn,
     currentTimeOut: formattedOut,
     efficiency: efficiency
   });
-
-  // Call displayTimelog to update the log display
   displayTimelog();
 }
 
 function displayTimelog() {
-  const logs = timelog; // Retrieve the array of timelog objects
+  const logs = timelog;
   const container = document.getElementById('timelog-container');
-
-  // Create a table or any other structure to display the timelog entries
   const table = document.createElement('table');
-  if (timelog.length == 1){
-  table.innerHTML = `
+  if (timelog.length == 1) {
+    table.innerHTML = `
     <tr>
       <th>Time In</th>
       <th>Time Out</th>
@@ -244,28 +228,36 @@ function displayTimelog() {
   }
 
   if (logs.length > 0) {
-    const lastLog = logs[logs.length - 1]; // Access the last item in the array
-    
+    const lastLog = logs[logs.length - 1];
     const row = document.createElement('tr');
     row.innerHTML = `
       <td>${lastLog.currentTimeIn}</td>
       <td>${lastLog.currentTimeOut}</td>
       <td>${lastLog.efficiency}</td>
     `;
-    table.appendChild(row); // Append only the last row
+    table.appendChild(row);
   }
 
   container.appendChild(table);
 }
-function formatTime(time){
-    const formattedDate = time.toLocaleString('en-US', {
-    month: 'numeric', // Numeric month
-    day: 'numeric', // Numeric day
-    year: 'numeric', // Numeric year
-    hour: 'numeric', // Numeric hour without leading zero
-    minute: '2-digit', // 2-digit minute
+
+function formatTime(time) {
+  const formattedDate = time.toLocaleString('en-US', {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
     second: '2-digit',
     hour12: false
   });
   return formattedDate;
+}
+
+function getTop(l) {
+  return l.map((a) => a.y).reduce((a, b) => Math.min(a, b));
+}
+
+function getMeanPosition(l) {
+  return l.map((a) => [a.x, a.y]).reduce((a, b) => [a[0] + b[0], a[1] + b[1]]).map((a) => a / l.length);
 }
