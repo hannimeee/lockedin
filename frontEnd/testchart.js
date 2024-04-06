740f35a79209e55ead6f4780bd800328d31e053c
Surya-SAT <165343982+Surya-Sathya@users.noreply.github.com>
2024-04-06 12:16:42 +1100

    fixed commenting

---
+++ frontEnd/testchart.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var timeArray = ['95', '76', '88', '91', '85']; // Hardcoded values for graph for now :(
+
+    let testChart = document.getElementById('testChart').getContext('2d');
+    Chart.defaults.global.defaultFontColor = 'rgb(256, 256, 256)'; 
+
+    let chart1 = new Chart(testChart, {
+        type:'line',
+        data:{
+            labels:['Log 1', 'Log 2', 'Log 3', 'Log 4', 'Log 5'],
+            datasets:[{
+                label:'Efficiency',
+                data: timeArray,
+                backgroundColor:['red','orange','yellow','green','blue','purple','pink']
+            }]
+        },
+        options:{}
+    });
+});
